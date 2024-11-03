12615dfe9cad9c71f2805f950a247a39685244fd
vojtech.kopal <kopalvojtech@gmail.com>
2024-11-03 19:04:55 +0100

    Major cleanup

--- src/loader/in_memory_loader.rs
+++ src/loader/in_memory_loader.rs
@@ -1,12 +1,11 @@
-use std::{collections::HashMap, sync::Arc};
+use std::collections::HashMap;
 
 use tokio::sync::RwLock;
 
 use super::{loader_trait::Loader, saved_file::SavedFile};
 
-#[derive(Clone)]
 pub struct InMemoryLoader  {
-    storage : HashMap<String, Arc<RwLock<SavedFile>>>
+    storage : HashMap<String, RwLock<SavedFile>>
 }
 
 impl Loader for InMemoryLoader {
@@ -19,14 +18,10 @@ impl Loader for InMemoryLoader {
     }
 
     fn insert_new(&mut self, name : &String, mime : &String) {
-        self.storage.insert(name.clone(), Arc::new(RwLock::new(SavedFile::new(Vec::new(), mime.clone()))));
+        self.storage.insert(name.clone(), RwLock::new(SavedFile::new(Vec::new(), mime.clone())));
     }
 
-    fn get_mut(&mut self, name : &String) -> Option<&mut Arc<RwLock<SavedFile>>> {
-        self.storage.get_mut(name)
-    }
-
-    fn load(&self, name : &String) -> Option<& Arc<RwLock<SavedFile>>> {
+    fn load(&self, name : &String) -> Option<& RwLock<SavedFile>> {
         self.storage.get(name)
     }
 
--- src/loader/loader_trait.rs
+++ src/loader/loader_trait.rs
@@ -1,12 +1,10 @@
 use tokio::sync::RwLock;
-use std::sync::Arc;
 use super::saved_file::SavedFile;
 
 pub trait Loader {
     fn init() -> Self;
     fn exists(&self, name : &String) -> bool;
     fn insert_new(&mut self, name : &String, mime : &String);
-    fn get_mut(&mut self, name : &String) -> Option<&mut Arc<RwLock<SavedFile>>>;
-    fn load(&self, name : &String) -> Option<& Arc<RwLock<SavedFile>>>;
+    fn load(&self, name : &String) -> Option<& RwLock<SavedFile>>;
     fn delete(&mut self, name : &String) -> bool;
 }
